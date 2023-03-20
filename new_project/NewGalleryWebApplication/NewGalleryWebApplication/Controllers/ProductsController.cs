3a66e879fc859c9c183514d1395da6ab102115ee
TheScientist-1 <natalia_muska@knu.ua>
2023-03-21 01:53:24 +0200

    .

--- new_project/NewGalleryWebApplication/NewGalleryWebApplication/Controllers/ProductsController.cs
+++ new_project/NewGalleryWebApplication/NewGalleryWebApplication/Controllers/ProductsController.cs
@@ -18,12 +18,55 @@ namespace NewGalleryWebApplication.Controllers
             _context = context;
         }
 
-        // GET: Products
-        public async Task<IActionResult> Index()
+        //// GET: Products
+        //public async Task<IActionResult> Index(int id, string name)
+        //{
+        //    ViewBag.CategoryId = id;
+        //    ViewBag.CategoryName = name;
+        //    var dbgalleryContext = _context.Products.Where(p => p.CategoryId == id).Include(p => p.Category);
+        //    return View(await dbgalleryContext.ToListAsync());
+        //}
+        public async Task<IActionResult> Index(string sortOrder, string searchString, int id, string name)
         {
-            var dbgalleryContext = _context.Products.Include(p => p.Category);
-            return View(await dbgalleryContext.ToListAsync());
+
+            ViewData["CurrentSort"] = sortOrder;
+            ViewData["NameSortParm"] = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewData["PriceSortParm"] = sortOrder == "Price" ? "price_desc" : "Price";
+
+
+            ViewBag.CategoryId = id;
+            ViewBag.CategoryName = name;
+
+            ViewData["CurrentFilter"] = searchString;
+            var dbgalleryContext = _context.Products.Where(p => p.CategoryId == id).Include(p => p.Category);
+            var products = from p in _context.Products.Where(a => a.CategoryId == id).Include(a => a.Category)
+                           select p;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                products = products.Where(p => p.Name.Contains(searchString));
+            }
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    products = products.OrderByDescending(p => p.Name);
+                    break;
+
+                case "Price":
+                    products = products.OrderBy(p => p.Price);
+                    break;
+                case "price_desc":
+                    products = products.OrderByDescending(p => p.Price);
+                    break;
+                default:
+                    products = products.OrderBy(p => p.Name);
+                    break;
+            }
+
+            return View(await products.AsNoTracking().ToListAsync());
         }
+
 
         // GET: Products/Details/5
         public async Task<IActionResult> Details(int? id)
@@ -44,27 +87,46 @@ namespace NewGalleryWebApplication.Controllers
             return View(product);
         }
 
+        //// GET: Products/Create
+        //public IActionResult Create(int categoryId)
+        //{
+        //    //ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name");
+        //    ViewBag.CategoryId = categoryId;
+        //    ViewBag.CategoryName = _context.Categories.Where(p => p.Id == categoryId).FirstOrDefault().Name;
+        //    return View();
+        //}
+
+
         // GET: Products/Create
         public IActionResult Create()
         {
-            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Id");
             return View();
         }
 
         // POST: Products/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,CategoryId,Price,Info")] Product product)
+        public async Task<IActionResult> Create(int categoryId, [Bind("Name,CategoryId,Price,Info")] Product product)
         {
-            if (ModelState.IsValid)
+            product.CategoryId = categoryId;
+            try
             {
-                _context.Add(product);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                if (ModelState.IsValid)
+                {
+                    _context.Add(product);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
             }
-            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Id", product.CategoryId);
+            catch (DbUpdateException /* ex */)
+            {
+                //Log the error (uncomment ex variable name and write a log.
+                ModelState.AddModelError("", "Unable to save changes. " +
+                    "Try again, and if the problem persists " +
+                    "see your system administrator.");
+            }
             return View(product);
         }
 
@@ -81,16 +143,17 @@ namespace NewGalleryWebApplication.Controllers
             {
                 return NotFound();
             }
-            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Id", product.CategoryId);
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", product.CategoryId);
             return View(product);
         }
+
 
         // POST: Products/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,CategoryId,Price,Info")] Product product)
+        public async Task<IActionResult> Edit(int id, [Bind("Name,CategoryId,Price,Info")] Product product)
         {
             if (id != product.Id)
             {
@@ -117,7 +180,7 @@ namespace NewGalleryWebApplication.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Id", product.CategoryId);
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", product.CategoryId);
             return View(product);
         }
 
