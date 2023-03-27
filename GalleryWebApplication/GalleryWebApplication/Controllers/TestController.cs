a626a77b63ddbb10b28634b72695a1b18ba56ad5
TheScientist-1 <108238495+TheScientist-1@users.noreply.github.com>
2023-03-27 19:06:44 +0300

    add controller for photo uploading
    add controller for photo uploading
    
    and
    
    cat
    
    photo

---
+++ GalleryWebApplication/GalleryWebApplication/Controllers/TestController.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GalleryWebApplication;
+using GalleryWebApplication.Models.GalleryViewModels;
+
+namespace GalleryWebApplication.Controllers
+{
+    public class TestController : Controller
+    {
+
+
+        public TestController()
+        {
+
+        }
+
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult UploadPhoto()
+        {
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult UploadPhoto(IFormFile photo)
+        {
+            if (photo == null || photo.Length == 0)
+            {
+                return BadRequest("Invalid file");
+            }
+
+            var fileName = Path.GetFileName(photo.FileName);
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "photos", fileName);
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                photo.CopyTo(stream);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
